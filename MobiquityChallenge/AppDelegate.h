7f942b9932008f6574385fcee0f279278a4f232a
Chad Armstrong <chad@edenwaith.com>
2014-11-07 10:40:40 -0600

    Initial Commit

---
+++ MobiquityChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobiquityChallenge
+//
+//  Created by Chad Armstrong on 11/7/14.
+//  Copyright (c) 2014 Chad Armstrong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
